21628ccbf8a9a6423b3d932f9a6fdad8b7f0f1c9
nb <bneuhausz@gmail.com>
2025-03-25 16:41:49 +0100

    added do this one skeleton

---
+++ src/app/do-this-one/do-this-one.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-do-this-one',
+  imports: [],
+  template: `
+    <p>
+      do-this-one
+    </p>
+  `,
+  styles: ``
+})
+export default class DoThisOneComponent {
+
+}
