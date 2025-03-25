d33d977a55d9e6092f818f847f5530ffe407e817
nb <bneuhausz@gmail.com>
2025-03-25 18:22:45 +0100

    minor nav formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   imports: [RouterOutlet, RouterLink],
   template: `
     <nav>
-      <a routerLink="/do-not-do-this">Do Not Do This</a>
-      <a routerLink="/do-this-one">Do This One</a>
-      <a routerLink="/do-this-two">Do This Two</a>
+      <a routerLink="/do-not-do-this">Do Not Do This</a> -
+      <a routerLink="/do-this-one">Do This One</a> -
+      <a routerLink="/do-this-two">Do This Two</a> -
       <a routerLink="/do-this-three">Do This Three</a>
     </nav>
 
